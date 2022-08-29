714000523d95a6df2891c7f47c033935c12ad1e4
yuan <2541093882@qq.com>
2022-08-29 08:57:16 +0800

    委托

--- HiKoltin/src/Kotlin基础/pkg15_委托/p2_属性委托/l1_延迟属性/HiKotlin.kt
+++ HiKoltin/src/Kotlin基础/pkg15_委托/p2_属性委托/l1_延迟属性/HiKotlin.kt
@@ -1,4 +1,4 @@
-package Kotlin基础.pkg15_委托.p2_属性委托
+package Kotlin基础.pkg15_委托.p2_属性委托.l1_延迟属性
 
 // 2.委托属性 -- 延迟属性: 第1次访问属性的时候，会执行一次计算，后面再次访问的时候，就不再执行计算了，而是使用第一次缓存的结果
 val myLazyValue: Int by lazy {
@@ -11,8 +11,8 @@ val myLazyValue: Int by lazy {
 // (3) 返回值: 函数返回 Lazy<T>，作为委托的结果
 
 
-// LazyThreadSafetyMode:
-// SYNCHRONIZED: 默认情况下，延迟属性的计算是同步的: 值只会在一个线程中得到计算，所有的线程都会使用相同的一个结果
+// lazy重载方法中的另外一个参数: mode:LazyThreadSafetyMode:
+// SYNCHRONIZED: 默认情况下，延迟属性的计算是同步的: 值只会在一个线程中得到计算，所有的线程都会使用相同的结果值
 // PUBLICATION: 如果不需要初始化委托同步的，这个多线程可以同步执行
 // NONE: 如果确定初始化操作只会在一个线程中执行，这样会减少线程安全方面的开销
 
